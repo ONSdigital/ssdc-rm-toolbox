799f46a318c5c997b20f4d1e8cbfa255acff20c7
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-07-16 13:39:34 +0100

    Update run_in_kubernetes.sh

--- run_in_kubernetes.sh
+++ run_in_kubernetes.sh
@@ -11,7 +11,7 @@ kubectl run toolbox -it --rm \
    --image $IMAGE\
    --env=RABBITMQ_USER=$(kubectl get secret rabbitmq -o=jsonpath="{.data.rabbitmq-username}" | base64 --decode) \
    --env=RABBITMQ_PASSWORD=$(kubectl get secret rabbitmq -o=jsonpath="{.data.rabbitmq-password}" | base64 --decode) \
-   --env=RABBITMQ_SERVICE_HOST=rabbitmq \
+   --env=RABBITMQ_SERVICE_HOST=rm-rabbitmq \
    --env=RABBITMQ_SERVICE_PORT=5672 \
    --env=RABBITMQ_HTTP_PORT=15672 \
    --env=RABBITMQ_VHOST='/' \
