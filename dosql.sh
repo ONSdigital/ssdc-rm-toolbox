c3c347245dd42de2573bc639ae1b6d52f079b59a
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-10-11 10:31:48 +0100

    Protect dosql with readonly transaction mode

--- dosql.sh
+++ dosql.sh
@@ -3,4 +3,4 @@ if [[ -z "$1" ]]; then
 else
   username=$1
 fi
-psql "sslmode=require hostaddr=$DB_HOST user=$username dbname=$DB_NAME" --no-psqlrc -e -L ~/.audit/$CURRENT_USER/sql_${username}_$(date --iso-8601=ns).log
+PGOPTIONS="-c default_transaction_read_only=true" psql "sslmode=require hostaddr=$DB_HOST user=$username dbname=$DB_NAME" --no-psqlrc -e -L ~/.audit/$CURRENT_USER/sql_${username}_$(date --iso-8601=ns).log
